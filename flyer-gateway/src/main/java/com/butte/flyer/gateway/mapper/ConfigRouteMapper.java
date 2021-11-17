d1110c4566823007fd464b33d05da5c0f17da9fb
cicadasmile <1327880701@qq.com>
2021-11-17 20:42:53 +0800

    butte-frame应用层案例

---
+++ flyer-gateway/src/main/java/com/butte/flyer/gateway/mapper/ConfigRouteMapper.java
@@ -0,0 +1,15 @@
+package com.butte.flyer.gateway.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.butte.flyer.gateway.entity.ConfigRoute;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 路由管理Mapper层
+ * @author 公众号:知了一笑
+ * @since 2021-11-14 18:16
+ */
+@Mapper
+public interface ConfigRouteMapper extends BaseMapper<ConfigRoute> {
+
+}
