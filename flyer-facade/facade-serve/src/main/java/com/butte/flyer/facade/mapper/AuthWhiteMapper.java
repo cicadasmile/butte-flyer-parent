51e5d7e7fb2421229db27734178017b9efc85235
cicadasmile <1327880701@qq.com>
2021-11-18 21:02:08 +0800

    butte-frame应用层案例

---
+++ flyer-facade/facade-serve/src/main/java/com/butte/flyer/facade/mapper/AuthWhiteMapper.java
@@ -0,0 +1,15 @@
+package com.butte.flyer.facade.mapper;
+
+import com.butte.flyer.facade.entity.PO.AuthWhite;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 授权白名单 Mapper 接口
+ * @author 公众号:知了一笑
+ * @since 2021-09-05 17:32
+ */
+@Mapper
+public interface AuthWhiteMapper extends BaseMapper<AuthWhite> {
+
+}
