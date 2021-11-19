bf36ba14336e861f92cf4702fefc5152f25cd6f1
cicadasmile <1327880701@qq.com>
2021-11-19 20:29:24 +0800

    butte-frame应用层案例

---
+++ flyer-admin/admin-serve/src/main/java/com/butte/flyer/admin/entity/PO/SysUser.java
@@ -0,0 +1,57 @@
+package com.butte.flyer.admin.entity.PO;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import java.util.Date;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * 用户基础表
+ * @author 公众号:知了一笑
+ * @since 2021-09-05 15:25
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("sys_user")
+@ApiModel(value="User对象", description="系统用户表")
+public class SysUser implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+    @ApiModelProperty(value = "主键ID")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty(value = "用户名称")
+    private String userName;
+
+    @ApiModelProperty(value = "账号密码")
+    private String passWord;
+
+    @ApiModelProperty(value = "手机号")
+    private String phone;
+
+    @ApiModelProperty(value = "邮箱")
+    private String email;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+    @ApiModelProperty(value = "状态-1启用，2禁用，3删除")
+    private Integer state;
+
+    @ApiModelProperty(value = "备注信息")
+    private String remark;
+
+}
