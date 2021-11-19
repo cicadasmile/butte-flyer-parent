90f77a67619d2fddc4204808028b6a3929af49e2
cicadasmile <1327880701@qq.com>
2021-11-19 20:27:58 +0800

    butte-frame应用层案例

---
+++ flyer-admin/admin-serve/src/main/java/com/butte/flyer/admin/controller/kafka/KafkaTopic.java
@@ -0,0 +1,14 @@
+package com.butte.flyer.admin.controller.kafka;
+
+/**
+ * Kafka常量
+ * @author 公众号:知了一笑
+ * @since 2021-10-31 16:34
+ */
+public class KafkaTopic {
+
+    public static final String USER_TOPIC = "account-user-topic";
+
+    public static final String USER_INSERT_KEY = "insert-user" ;
+
+}
