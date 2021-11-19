c0dcc85f3688bc025dbf656993e033cd139716c2
cicadasmile <1327880701@qq.com>
2021-11-19 20:27:45 +0800

    butte-frame应用层案例

---
+++ flyer-admin/admin-serve/src/main/java/com/butte/flyer/admin/constant/ApiConstant.java
@@ -0,0 +1,12 @@
+package com.butte.flyer.admin.constant;
+
+/**
+ * 接口白名单
+ * @author 公众号:知了一笑
+ * @since 2021-10-17 11:14
+ */
+public class ApiConstant {
+
+    public static final String[] apiArr = new String[]{"/sys/user/login"} ;
+
+}
