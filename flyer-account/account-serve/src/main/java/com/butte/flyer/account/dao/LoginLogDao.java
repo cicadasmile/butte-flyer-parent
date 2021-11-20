80343464218b9949b68d9491bcc0c6efda7a4ee0
cicadasmile <1327880701@qq.com>
2021-11-20 12:30:57 +0800

    butte-frame应用层案例

---
+++ flyer-account/account-serve/src/main/java/com/butte/flyer/account/dao/LoginLogDao.java
@@ -0,0 +1,13 @@
+package com.butte.flyer.account.dao;
+
+import com.butte.flyer.account.entity.PO.LoginLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 登录日志表Dao层
+ * @author 公众号:知了一笑
+ * @since 2021-09-09 20:45
+ */
+public interface LoginLogDao extends IService<LoginLog> {
+
+}
