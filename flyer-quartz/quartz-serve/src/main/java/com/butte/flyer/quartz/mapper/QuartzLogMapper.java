0144f9b17eeecd3da50613318685e646ff621526
cicadasmile <1327880701@qq.com>
2021-11-21 09:52:34 +0800

    butte-frame应用层案例

---
+++ flyer-quartz/quartz-serve/src/main/java/com/butte/flyer/quartz/mapper/QuartzLogMapper.java
@@ -0,0 +1,10 @@
+package com.butte.flyer.quartz.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.butte.quartz.entity.QuartzLog;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuartzLogMapper extends BaseMapper<QuartzLog> {
+
+}
